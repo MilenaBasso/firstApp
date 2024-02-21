f09b0e5a5bb19f437055f65da15c92b124a5c43f
Milena <milena.basso444@gmail.com>
2024-02-21 10:45:08 -0300

    update

--- App.js
+++ App.js
@@ -2,21 +2,28 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={{backgroundColor: 'black', color: 'white', fontSize: 60}}>Peaceful Space</Text>
-      <Text style={{backgroundColor: 'black', color: 'white', fontSize: 40}}>Take a break to breathe and relax</Text>
-      <Text style={{color: 'white', fontSize: 26}}>...</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+return (
+<View style={styles.container}>
+<View style={{ backgroundColor: 'gray', width: '100%', height: 60 }}>
+</View>
+<View style={styles.secondaryContainer}>
+<Text>Testing</Text>
+</View>
+<View style={{ backgroundColor: 'gray', width: '100%', height: 60 }}>
+</View>
+</View>
+);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'black',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+container: {
+flex: 1,
+},
+secondaryContainer: {
+flex: 1,
+width: '100%',
+alignItems: 'flex-end',
+justifyContent: 'flex-end',
+backgroundColor: '#fff',
+},
 });
