9ebc4ff2de2ba9de513a956f270e04efb0d1b08b
Milena <milena.basso444@gmail.com>
2024-02-21 10:15:38 -0300

    started project

--- App.js
+++ App.js
@@ -4,7 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{backgroundColor: 'black', color: 'white', fontSize: 60}}>Peaceful Space</Text>
+      <Text style={{backgroundColor: 'black', color: 'white', fontSize: 40}}>Take a break to breathe and relax</Text>
+      <Text style={{color: 'white', fontSize: 26}}>...</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +15,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
   },
