5cfd3bc17b37be96ec9b00d6ba4f9611ca73b24f
Milena <milena.basso444@gmail.com>
2024-02-21 12:45:16 -0300

    atividade finalizada

--- App.js
+++ App.js
@@ -7,7 +7,9 @@ return (
 <View style={{ backgroundColor: 'gray', width: '100%', height: 60 }}>
 </View>
 <View style={styles.secondaryContainer}>
-<Text>Testing</Text>
+<Text style= {{ color: 'blue', fontSize: 30, fontWeight: 'bold', textAlign: 'end'}}>Milena Basso</Text>
+<Text style= {{ color: 'black', fontStyle: 'italic', fontSize: 25, textAlign: 'start', borderWidth: 1, borderColor: 'purple'}}>Note 27</Text>
+<Text style= {{ backgroundColor: 'pink',  fontSize: 20, textAlign: 'center'}}>Sala 07</Text>
 </View>
 <View style={{ backgroundColor: 'gray', width: '100%', height: 60 }}>
 </View>
@@ -19,11 +21,11 @@ const styles = StyleSheet.create({
 container: {
 flex: 1,
 },
+
 secondaryContainer: {
 flex: 1,
 width: '100%',
-alignItems: 'flex-end',
-justifyContent: 'flex-end',
+justifyContent: 'center',
 backgroundColor: '#fff',
 },
 });
