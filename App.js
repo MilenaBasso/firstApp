34830db43c88f356a82cf23d6bc247be60f523d3
Milena <milena.basso444@gmail.com>
2024-02-23 10:55:25 -0300

    S1-02A finished activity

--- App.js
+++ App.js
@@ -1,46 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { ImageBackground } from 'react-native-web';
 
 export default function App() {
-
-    pressButon = () => {
-        alert("Você apertou o botão")
-    }
-
-    const imagem = { uri: 'https://urldaimagem.com' }
-
     return (
         <View style={styles.container}>
-
-
-
-            <Image
-                source={imagem}
-            />
-
-            <Image
-                source={require('./assets/favicon.png')}
-                style={[styles.img, { resizeMode: "cover" }]}
-            />
-
-
-            <ImageBackground>
-
-            </ImageBackground>
-
-
             <View style={{ backgroundColor: 'gray', width: '100%', height: 60 }}>
             </View>
             <View style={styles.secondaryContainer}>
-
-                <TextInput style={styles.inputName}
-                    placeholder='Digite seu Nome'
-                    inputMode='numeric'
-                    secureTextEntry={true}
-                    placeholderTextColor='pink'
-                    onChangeText={changeText}
-                />
 
                 <Text style={{ color: 'blue', fontSize: 30, fontWeight: 'bold', textAlign: 'end' }}>Milena Basso</Text>
                 <Text style={{ color: 'black', fontStyle: 'italic', fontSize: 25, textAlign: 'start', borderWidth: 1, borderColor: 'purple' }}>Note 27</Text>
@@ -48,18 +14,9 @@ export default function App() {
             </View>
             <View style={{ backgroundColor: 'gray', width: '100%', height: 60 }}>
             </View>
-
         </View>
     );
 }
-
-<Button
-    style={{ backgroundColor: 'red' }}
-    onPress={pressButon}
-    title='Click Me!'
-    color='pink'
-/>
-
 
 const styles = StyleSheet.create({
     container: {
@@ -69,6 +26,7 @@ const styles = StyleSheet.create({
     secondaryContainer: {
         flex: 1,
         width: '100%',
+
         justifyContent: 'center',
         backgroundColor: '#fff',
     },
