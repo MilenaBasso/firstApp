ef968fceab71d729afd5c0a7e20274a5bb8b3d09
Milena <milena.basso444@gmail.com>
2024-02-23 10:34:09 -0300

    messed

--- App.js
+++ App.js
@@ -1,43 +1,75 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { ImageBackground } from 'react-native-web';
 
 export default function App() {
 
-    
-return (
-<View style={styles.container}>
+    pressButon = () => {
+        alert("Você apertou o botão")
+    }
 
-    <TextInput style= {styles.inputName}
-    placeholder='Digite seu Nome'
-    inputMode='numeric'
-    secureTextEntry={true}
-    placeholderTextColor='pink'
-    onChangeText={changeText}
-    />
+    const imagem = { uri: 'https://urldaimagem.com' }
 
-{/* <View style={{ backgroundColor: 'gray', width: '100%', height: 60 }}>
-</View>
-<View style={styles.secondaryContainer}>
-<Text style= {{ color: 'blue', fontSize: 30, fontWeight: 'bold', textAlign: 'end'}}>Milena Basso</Text>
-<Text style= {{ color: 'black', fontStyle: 'italic', fontSize: 25, textAlign: 'start', borderWidth: 1, borderColor: 'purple'}}>Note 27</Text>
-<Text style= {{ backgroundColor: 'pink',  fontSize: 20, textAlign: 'center'}}>Sala 07</Text>
-</View>
-<View style={{ backgroundColor: 'gray', width: '100%', height: 60 }}>
-</View> */}
+    return (
+        <View style={styles.container}>
 
-</View>
-);
+
+
+            <Image
+                source={imagem}
+            />
+
+            <Image
+                source={require('./assets/favicon.png')}
+                style={[styles.img, { resizeMode: "cover" }]}
+            />
+
+
+            <ImageBackground>
+
+            </ImageBackground>
+
+
+            <View style={{ backgroundColor: 'gray', width: '100%', height: 60 }}>
+            </View>
+            <View style={styles.secondaryContainer}>
+
+                <TextInput style={styles.inputName}
+                    placeholder='Digite seu Nome'
+                    inputMode='numeric'
+                    secureTextEntry={true}
+                    placeholderTextColor='pink'
+                    onChangeText={changeText}
+                />
+
+                <Text style={{ color: 'blue', fontSize: 30, fontWeight: 'bold', textAlign: 'end' }}>Milena Basso</Text>
+                <Text style={{ color: 'black', fontStyle: 'italic', fontSize: 25, textAlign: 'start', borderWidth: 1, borderColor: 'purple' }}>Note 27</Text>
+                <Text style={{ backgroundColor: 'pink', fontSize: 20, textAlign: 'center' }}>Sala 07</Text>
+            </View>
+            <View style={{ backgroundColor: 'gray', width: '100%', height: 60 }}>
+            </View>
+
+        </View>
+    );
 }
 
-const styles = StyleSheet.create({
-container: {
-flex: 1,
-},
+<Button
+    style={{ backgroundColor: 'red' }}
+    onPress={pressButon}
+    title='Click Me!'
+    color='pink'
+/>
 
-secondaryContainer: {
-flex: 1,
-width: '100%',
-justifyContent: 'center',
-backgroundColor: '#fff',
-},
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+
+    secondaryContainer: {
+        flex: 1,
+        width: '100%',
+        justifyContent: 'center',
+        backgroundColor: '#fff',
+    },
 });
